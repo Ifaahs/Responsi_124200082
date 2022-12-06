be6a522fa625000bea092809a1923b9da8bb9a21
Ifaahs <syarifahinsan21@gmail.com>
2022-12-06 19:16:06 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+//Nama : Syarifah Insan Sekartini
+//NIM : 124200082
+
+import 'package:flutter/material.dart';
+import 'package:responsi/menu.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+
+      ),
+      home: const MenuPage(),
+    );
+  }
+}
